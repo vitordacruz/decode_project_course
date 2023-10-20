70c8370573f888f8c867c181696d5e2b5c100055
Vitor da Cruz <vitor.c.brandao@accenture.com>
2023-10-20 16:35:03 -0300

    ADD Models and Repositories

--- src/main/java/com/ead/course/models/ModuleModel.java
+++ src/main/java/com/ead/course/models/ModuleModel.java
@@ -1,7 +1,5 @@
 package com.ead.course.models;
 
-import com.ead.course.enums.CourseLevel;
-import com.ead.course.enums.CourseStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
@@ -14,35 +12,20 @@ import java.util.UUID;
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
-@Table(name = "TB_COURSES")
-public class CourseModel implements Serializable {
+@Table(name = "TB_MODULES")
+public class ModuleModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @org.hibernate.annotations.Type(type="uuid-char")
-    private UUID courseID;
+    private UUID moduleID;
     @Column(nullable = false, length = 150)
-    private String name;
+    private String title;
     @Column(nullable = false, length = 250)
     private String description;
-    @Column
-    private String imageUrl;
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
-    @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
-    private LocalDateTime lastUpdateDate;
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private CourseStatus courseStatus;
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private CourseLevel courseLevel;
-    @Column(nullable = false)
-    @org.hibernate.annotations.Type(type="uuid-char")
-    private UUID userInstructor;
-
 
 }
