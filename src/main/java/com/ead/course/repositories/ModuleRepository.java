d19c86f05318a9e6da4fde9faef70a6138aaeb96
Vitor da Cruz <vitor.c.brandao@accenture.com>
2023-10-24 17:36:34 -0300

    Added Module Rest API

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -7,12 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
     @EntityGraph(attributePaths = {"course"})
     ModuleModel findByTitle(String title);
 
-    @Query(value = "select * from tb_modules where course_courseid = :courseId", nativeQuery = true)
+    @Query(value = "from ModuleModel m where m.course.courseID = :courseId")
     List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
+    @Query(value = "from ModuleModel m where m.course.courseID = :courseId and m.moduleID = :moduleId")
+    Optional<ModuleModel> findModuleIntoCourse(@Param("courseId") UUID courseId, @Param("moduleId") UUID moduleId);
 }
